8747ac18f0b5fe768f0fc6d0092745232ddce81f
APDCeroca <alexprieto@ceroca.cat>
2026-02-19 16:54:09 +0100

    Colores nuevos, pagina login creada

--- lib/coloresapp.dart
+++ lib/coloresapp.dart
@@ -4,7 +4,7 @@ class Coloresapp {
   Color colorFondo = Color.fromARGB(255, 242, 242, 242);
   Color colorPrimario = Color.fromARGB(255, 204, 75, 58);
   Color colorPrimarioAccentuado = Color.fromARGB(255, 225, 139, 129);
-  Color colorTexto = Color.fromARGB(255, 102, 102, 102);
-  Color ColorTextoFlojo = Color.fromARGB(255, 51, 51, 51);
+  Color colorTexto = Color.fromARGB(255, 51, 51, 51);
+  Color ColorTextoFlojo = Color.fromARGB(255, 102, 102, 102);
   Color colorContorno = Color.fromARGB(255, 75, 75, 75);
 }
