e7cb57bc87c8ad9783e23be2f15c38d4c645fb7a
APDCeroca <alexprieto@ceroca.cat>
2026-02-19 19:05:12 +0100

    Colores y textfield arreglados

--- lib/coloresapp.dart
+++ lib/coloresapp.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Coloresapp {
-  Color colorFondo = Color.fromARGB(255, 242, 242, 242);
-  Color colorPrimario = Color.fromARGB(255, 204, 75, 58);
-  Color colorPrimarioAccentuado = Color.fromARGB(255, 225, 139, 129);
-  Color colorTexto = Color.fromARGB(255, 51, 51, 51);
-  Color ColorTextoFlojo = Color.fromARGB(255, 102, 102, 102);
-  Color colorContorno = Color.fromARGB(255, 75, 75, 75);
+  static Color colorFondo = Color.fromARGB(255, 242, 242, 242);
+  static Color colorPrimario = Color.fromARGB(255, 207, 75, 58);
+  static Color colorPrimarioAccentuado = Color.fromARGB(255, 225, 139, 129);
+  static Color colorTexto = Color.fromARGB(255, 51, 51, 51);
+  static Color ColorTextoFlojo = Color.fromARGB(255, 102, 102, 102);
+  static Color colorContorno = Color.fromARGB(255, 75, 75, 75);
+  //Color colorFondo = Color.fromARGB(255, 242, 242, 242); Color colorPrimario = Color.fromARGB(255, 204, 75, 58); Color colorPrimarioAccentuado = Color.fromARGB(255, 225, 139, 129); Color colorTexto = Color.fromARGB(255, 102, 102, 102); Color ColorTextoFlojo = Color.fromARGB(255, 51, 51, 51); Color colorContorno = Color.fromARGB(255, 75, 75, 75);
 }
