e4720f6992c9e65fbe7eedc4636f3c13fb64dea3
jialeitor <jia_xia@ceroca.cat>
2026-03-26 16:39:51 +0100

    El fondo arreglado para versión móvil

--- lib/Components/Login/texto_normal.dart
+++ lib/Components/Login/texto_normal.dart
@@ -19,6 +19,9 @@ class TextoNormal extends StatelessWidget {
         fontFamily: 'HoshikoSatsuki',
         fontSize: 24,
         letterSpacing: 2,
+        foreground: Paint()
+            ..blendMode = BlendMode.difference
+            ..color = Colors.white,
       ),
       
     );
